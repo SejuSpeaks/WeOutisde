c07c85b9211cd27988376dbe7ff0941dbdbe3cae
Jael <humanerror787@gmail.com>
2023-09-28 17:58:14 -0400

    ready for routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -10,9 +10,20 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
+router.get('/csrf/restore', (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-Token': csrfToken
+    })
+    // res.send('Hey there')
+})
+
+
 router.post('/test', (req, res) => {
     res.json({ RequestBody: req.body })
 })
+
 
 
 module.exports = router
