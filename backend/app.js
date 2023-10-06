d50162e7f08822c175f9a9db120061c661f1575e
Jael <humanerror787@gmail.com>
2023-10-06 08:14:23 -0400

    brought enviroment back

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { environment } = require('./config')
 const isProduction = environment === 'production';
 const routes = require('./routes');
 
